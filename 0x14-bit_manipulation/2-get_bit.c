70c2d9cd4f03941a0dd0241eed92314d71f6f0f5
jospeh007 <youssefgf123@gmail.com>
2023-10-26 14:57:40 -0700

    Task 3

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * git_bit - Get the value of a bit at a specific index in a decimal number.
+ * @n: The number to inspect.
+ * @index: The index of the bit to retrieve.
+ *
+ * Return: The value of the requested bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	return (n >> index & 1);
+}
