0b8c3ad4fcff3818694676c7fd331d44cf5fdbed
jospeh007 <youssefgf123@gmail.com>
2023-12-22 13:08:31 -0800

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,7 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (ht == NULL || ht->array == NULL)
 		return;
 
-	printf("(");
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
